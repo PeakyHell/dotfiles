4349cff6af5559f52288a94e308a7b21434901ef
PeakyHell <contact@peakyhell.com>
2025-08-26 22:40:24 +0200

    changed Brewfile to a script

---
+++ MacOs/homebrew.sh
@@ -0,0 +1,17 @@
+# Install packages
+brew install gcc
+brew install git
+brew install neovim
+brew install node
+brew install openjdk
+brew install python
+
+# Install casks
+brew install --cask --appdir="~/Applications" discord
+brew install --cask --appdir="~/Applications" docker
+brew install --cask --appdir="~/Applications" kitty
+brew install --cask --appdir="~/Applications" librewolf --no-quarantine
+brew install --cask --appdir="~/Applications" obsidian
+brew install --cask --appdir="~/Applications" protonvpn
+brew install --cask --appdir="~/Applications" scroll-reverser
+brew install --cask --appdir="~/Applications" spotify
