123a6f7101ea82e2f22dfd8d05ec16725053fc02
PeakyHell <contact@peakyhell.com>
2025-12-10 17:26:30 +0100

    [OhMyZsh] Add you-should-use plugin

--- Scripts/windows_methods.sh
+++ Scripts/windows_methods.sh
@@ -100,6 +100,7 @@ windows_config_files() {
 	# OhMyZsh
 	# git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 	# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+	# git clone https://github.com/MichaelAquilina/zsh-you-should-use.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/you-should-use
 
 	# Fastfetch config
 	# mkdir -p ~/.config/fastfetch
