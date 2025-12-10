7c764c0b689981256dc95209c1ad5c0769c0fc4f
PeakyHell <contact@peakyhell.com>
2025-12-10 17:23:17 +0100

    [OhMyZsh] Add zsh-syntax-highlighting plugin

--- Scripts/arch_methods.sh
+++ Scripts/arch_methods.sh
@@ -92,6 +92,7 @@ arch_config_files() {
 
 	# OhMyZsh
 	git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 	# Fastfetch config
 	mkdir -p ~/.config/fastfetch
--- Scripts/macos_methods.sh
+++ Scripts/macos_methods.sh
@@ -140,6 +140,7 @@ macos_config_files() {
 
 	# OhMyZsh
 	git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 	# Fastfetch config
 	mkdir -p ~/.config/fastfetch
