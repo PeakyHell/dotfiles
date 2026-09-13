97c4a7d94f06b8afe8646e73ca03546ad2d95bd7
PeakyHell <contact@peakyhell.com>
2026-09-13 21:59:13 +0200

    Add grim and slurp to config for screenshots

--- Arch/home/.config/hypr/hyprland.lua
+++ Arch/home/.config/hypr/hyprland.lua
@@ -324,6 +324,7 @@ hl.bind("XF86AudioPause", hl.dsp.exec_cmd("playerctl play-pause"), { locked = tr
 hl.bind("XF86AudioPlay",  hl.dsp.exec_cmd("playerctl play-pause"), { locked = true })
 hl.bind("XF86AudioPrev",  hl.dsp.exec_cmd("playerctl previous"),   { locked = true })
 
+hl.bind("Print", hl.dsp.exec_cmd('grim -g "$(slurp)" - | wl-copy'))
 
 --------------------------------
 ---- WINDOWS AND WORKSPACES ----
